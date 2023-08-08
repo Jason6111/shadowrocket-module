fa13b66387e84c786a2e595de9b99203f92b688a
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-08-08 18:42:44 +0800

    Update lsp.js

--- QuantumultX/lsp.js
+++ QuantumultX/lsp.js
@@ -1,10 +1,8 @@
 #!name=lsp
 #!desc=仅供学习交流
 
-[mitm]
 hostname = *50a*,*yiqiapi*,*jiujiao*,*myb*api*,*hichatapi*,*tbrapi*,*wmqapi*,*gvlan*,*91av*,*xtt*
 
-[rewrite_local]
 ^http[s]?:\/\/.*((50(aa|ab|apia)pi|xtt.*)\.com|tbrapi\.org)\/pwa\.php\/api\/(user\/userinfo|MvDetail\/(detail|xiao_detail)|home\/(getOpenAdsAndVersion|appcenter)|system\/getad)$ url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/lsp.js
 ^http[s]?:\/\/.*(yiqiapi|jiujiao|myb[0-9]+api|wmqapi|i91av).*\/pwa\.php$ url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/lsp.js
 ^http[s]?:\/\/.*hichatapi.+\/api\.php$ url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Scripts/lsp.js
